a11dd866f0b2a14dca6c45fbbeba214b0df91b38
Dmitriy <dmitriy250300@mail.ru>
2023-05-30 18:51:45 +0300

    30.05.2023

--- 1140game.py
+++ 1140game.py
@@ -19,10 +19,15 @@ class TextArea:
         self.rect = pygame.Rect(x, y, width, height)
         '''rectangle - прямоугольник'''
         self.fill_color = color
+        # список для хранения вопросов
+        self.titles = []
 
-    def set_text(self, text, fsize, text_color=BLACK):
-        self.text = text
-        self.image = pygame.font.Font(None, fsize).render(text, True, text_color)
+    def add_text(self, text):
+        self.titles.append(text)
+
+    def set_text(self, number, fsize, text_color=BLACK):
+        self.text = self.titles[number]
+        self.image = pygame.font.Font(None, fsize).render(self.text, True, text_color)
 
     def draw(self, shift_x, shift_y):
         pygame.draw.rect(mw, self.fill_color, self.rect)
@@ -30,11 +35,24 @@ class TextArea:
 
 
 quest_card = TextArea(120, 100, 350, 70, LIGHT_BLUE)
-quest_card.set_text('Вопрос', 75)
+
+quest_card.add_text('Вопрос')
+quest_card.add_text('Что изучаешь в Алгоритмике?')
+quest_card.add_text('Какое твоё увлечение?')
+quest_card.add_text('На каком языке общаются в Испании?')
+quest_card.add_text('Что едят на ужин?')
+quest_card.add_text('Что падает с неба?')
+quest_card.set_text(0, 75)
 
 
 ans_card = TextArea(120, 250, 350, 70, LIGHT_BLUE)
-ans_card.set_text('Ответ', 75)
+ans_card.add_text('Ответ')
+ans_card.add_text('Python')
+ans_card.add_text('Французский')
+ans_card.add_text('Яблоки')
+ans_card.add_text('Капли дождя')
+ans_card.add_text('Жаркое с грибами')
+ans_card.set_text(0, 75)
 
 quest_card.draw(10, 10)
 ans_card.draw(10, 10)
@@ -44,23 +62,15 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                num = randint(1, 3)
-                if num == 1:
-                    quest_card.set_text('Что изучаешь в Алгоритмике?', 25)
-                if num == 2:
-                    quest_card.set_text('На каком языке говорят во Франции?', 25)
-                if num == 3:
-                    quest_card.set_text('Что растёт на яблоне?', 35)
+                num = randint(1, len(quest_card.titles)-1)
+                quest_card.set_text(num, 25)
+
                 quest_card.draw(10, 25)
 
             if event.key == pygame.K_a:
-                num = randint(1, 3)
-                if num == 1:
-                    ans_card.set_text('Python', 35)
-                if num == 2:
-                    ans_card.set_text('Французский', 35)
-                if num == 3:
-                    ans_card.set_text('Яблоки', 35)
+                num = randint(1, len(ans_card.titles) - 1)
+                ans_card.set_text(num, 25)
+
                 ans_card.draw(10, 25)
 
     clock.tick(40)
---
+++ indexs_lists.py
@@ -0,0 +1,6 @@
+from random import randint
+
+list1 = ['вопрос', 'раз', 'два', 'три', 'четыре', 'пять']
+             #0       1     2       3       4        5
+
+print(randint(1, len(list1)-1))
