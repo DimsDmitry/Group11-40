b1f05a97bf8e0362993cb5435ef55cebaae7cf62
Dmitriy <dmitriy250300@mail.ru>
2023-09-17 15:03:29 +0300

    17.09.2023

--- Fast_clicker.py
+++ Fast_clicker.py
@@ -1,17 +1,18 @@
 import pygame
 import time
-
+from random import randint
 
 pygame.init()
 '''создаём окно программы'''
-#цвет фона
+# цвет фона
 back = (244, 222, 253)
-#окно программы
+# окно программы
 mw = pygame.display.set_mode((500, 500))
 mw.fill(back)
 clock = pygame.time.Clock()
 
-#класс прямоугольник
+
+# класс прямоугольник
 class Area():
     def __init__(self, x, y, width, height, color):
         self.rect = pygame.Rect(x, y, width, height)
@@ -24,8 +25,11 @@ class Area():
         pygame.draw.rect(mw, self.fill_color, self.rect)
 
     def outline(self, frame_color, thickness):
-        #обводка прямоугольника
+        # обводка прямоугольника
         pygame.draw.rect(mw, frame_color, self.rect, thickness)
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
 
 
 class Label(Area):
@@ -39,6 +43,8 @@ class Label(Area):
 
 YELLOW = (255, 255, 0)
 BLUE = (80, 78, 231)
+GREEN = (0, 255, 96)
+RED = (254, 57, 35)
 
 cards = []
 num_cards = 4
@@ -51,3 +57,37 @@ for i in range(num_cards):
     cards.append(new_card)
     x += 100
 
+wait = 0
+
+while True:
+    if wait == 0:
+        # переносим надпись
+        wait = 20  # столько кадров надпись будет на одном месте
+        click = randint(0, num_cards - 1)
+        for i in range(num_cards):
+            cards[i].color(YELLOW)
+            if i == click:
+                cards[i].draw(10, 40)
+            else:
+                cards[i].fill()
+    else:
+        wait -= 1
+
+        # на каждом кадре проверяем клик
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = event.pos
+            for i in range(num_cards):
+                if cards[i].collidepoint(x, y):
+                    if i == click:
+                        # если на карте на которую мы кликнули, есть надпись -
+                        # перекрашиваем в зелёный, плюс очко
+                        cards[i].color(GREEN)
+                    else:
+                        # иначе перекрашиваем в красный, минус очко
+                        cards[i].color(RED)
+
+                    cards[i].fill()
+
+    pygame.display.update()
+    clock.tick(40)
