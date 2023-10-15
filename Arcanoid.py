9061d207b07206bd5ee3409a0b3fd6d14da6d03b
Dmitriy <dmitriy250300@mail.ru>
2023-10-15 17:22:14 +0300

    15.10.2023

--- Arcanoid.py
+++ Arcanoid.py
@@ -25,7 +25,7 @@ game_over = False
 
 # класс из предыдущего проекта
 class Area():
-    def __init__(self, x=0, y=0, width=10, height=10, color=None):
+    def __init__(self, x, y, width, height, color):
         self.rect = pygame.Rect(x, y, width, height)
         self.fill_color = back
         if color:
@@ -42,6 +42,16 @@ class Area():
 
     def colliderect(self, rect):
         return self.rect.colliderect(rect)
+
+
+# класс для надписей
+class Label(Area):
+    def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
+        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
+
+    def draw(self, shift_x=0, shift_y=0):
+        self.fill()
+        mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
 
 
 # класс для объектов-картинок
@@ -101,6 +111,20 @@ while not game_over:
     ball.rect.x += dx
     ball.rect.y += dy
 
+    # если координата Y мяча достигает 350 - поражение
+    if ball.rect.y > 350:
+        time_text = Label(150, 150, 50, 50, back)
+        time_text.set_text('ТЫ ПРОИГРАЛ', 60, (255, 0, 0))
+        time_text.draw(10, 10)
+        game_over = True
+
+    # закончились монстры - победа
+    if len(monsters) == 0:
+        time_text = Label(150, 150, 50, 50, back)
+        time_text.set_text('ПОБЕДА', 60, (28, 202, 0))
+        time_text.draw(10, 10)
+        game_over = True
+
     # если мяч достигает границы экрана, меняем его направление
     if ball.rect.y < 0:
         dy *= -1
@@ -113,6 +137,11 @@ while not game_over:
     # отрисовываем всех монстров из списка
     for m in monsters:
         m.draw()
+        # если мяч коснулся монстра - удаляем его из списка и меняем направление мяча
+        if m.rect.colliderect(ball.rect):
+            monsters.remove(m)
+            m.fill()
+            dy *= -1
 
     platform.draw()
     ball.draw()
